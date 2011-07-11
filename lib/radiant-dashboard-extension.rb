efdf9a62ec1b36b725172ccb888e80d20a78b743
spanner <will@spanner.org>
2011-07-11 11:24:21 +0100

    tinkering with shortcut presentation

--- lib/radiant-dashboard-extension.rb
+++ lib/radiant-dashboard-extension.rb
@@ -1,5 +1,5 @@
 module RadiantDashboardExtension
-  VERSION = '2.0.0.rc2'
+  VERSION = '2.0.0.rc3'
   SUMMARY = %q{Dashboard Extension for Radiant CMS}
   DESCRIPTION = %q{Dashboard provides a way to view recent activity in Radiant and gives small extensions a place to grow.}
   URL = "http://github.com/saturnflyer/radiant-dashboard-extension"
